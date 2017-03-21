233b1a1879ef6a8bd3f5baaecc7a96a09f7c79cd
Johnny <jegduke@yahoo.com>
2017-03-21 19:42:28 -0400

    Initial commit

---
+++ GitApp/app/src/main/java/pruebas/practica/com/example/servicios/gitapp/MainActivity.java
@@ -0,0 +1,13 @@
+package pruebas.practica.com.example.servicios.gitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
